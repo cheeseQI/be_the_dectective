55b4315466745c971dea42e1fe0d971d285d0601
Fangze Xu <fx33@duke.edu>
2023-03-25 18:45:18 -0400

    add chatdb

--- FirstDetect/DataModel/Chat.swift
+++ FirstDetect/DataModel/Chat.swift
@@ -7,17 +7,37 @@
 
 import Foundation
 
-class Chat: Identifiable, ObservableObject {
-    let id = UUID()
+class Chat: Identifiable, ObservableObject, Decodable {
+    //let id = UUID()
     var username: String = ""
     @Published var lastMessageText: String = ""
     @Published var messages: [Message] = []
     
     init(){}
     
+    enum CodingKeys : String, CodingKey {
+        case username = "username"
+        case lastMessageText = "lastMessageText"
+    }
+    
+    required init(from decoder: Decoder) throws{
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.username = try container.decode(String.self, forKey: .username)
+        self.lastMessageText = ""
+        self.messages = []
+    }
+    
     init(username: String, lastMessageText: String, messages: [Message]) {
         self.username = username
         self.lastMessageText = lastMessageText
         self.messages = messages
+    }
+    
+    func send(){
+        
+    }
+    
+    func recv(){
+        
     }
 }
