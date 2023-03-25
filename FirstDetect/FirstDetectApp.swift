fc9db0baa50c75fba6feb34d9ab07126392c13d9
Fangze Xu <fx33@duke.edu>
2023-03-25 16:31:23 -0400

    Initial Commit

---
+++ FirstDetect/FirstDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstDetectApp.swift
+//  FirstDetect
+//
+//  Created by oliver xu on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
