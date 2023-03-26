fdfcc766a21f996440e3ec3a45459cf8a1be6b74
sq44 <sq44@duke.edu>
2023-03-25 21:42:48 -0400

    test pass

--- back_end/mysite/game/views.py
+++ back_end/mysite/game/views.py
@@ -4,12 +4,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import openai
 
-openai.api_key = "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"
-openai.organization = "org-16Dqr6ZJhyzkIU8a1x2G4tIE"
+import configparser
+
+# 读取配置文件
+config = configparser.ConfigParser()
+config.read('/project/be_the_dectective/config.ini')
+# todo: need a separate api interface
+openai.api_key = config.get('key', 'key')
+openai.organization = config.get('org', 'org')
 
 conversation = [{"role": "system", "content": "You are the roles of Princess Dragomiroff in story"
                                               "Murder on the Orient Express, "
                                               "you need to answer my questions in her tongue"}]
+
+def start(request):
+    conversation.append({"role": "user", "content": input("user: ")})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=conversation
+    )
+    conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+    return HttpResponse(response['choices'][0]['message']['content'])
+
 
 def index(request):
     conversation.append({"role": "user", "content": input("user: ")})
