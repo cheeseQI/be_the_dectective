ca9dd8b66f86a62dbf5d044e0bae36db7b3aad89
Fangze Xu <fx33@duke.edu>
2023-03-26 01:51:54 -0400

    correct changes

--- FirstDetect/DataModel/ChatDB.swift
+++ FirstDetect/DataModel/ChatDB.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+let postChaturl = "http://vcm-30653.vm.duke.edu:8080/game/start"
+let postChosenScripturl = "http://vcm-30653.vm.duke.edu:8080/game/start"
+
 class ChatDB : ObservableObject{
     static let shared = ChatDB()
     @Published var record : [Chat]
@@ -15,54 +18,80 @@ class ChatDB : ObservableObject{
         self.record = [Chat]()
     }
     
-    func getchats(){
-        let url = URL(string: "https://ece564sp23.colab.duke.edu/entries/all")!
-        let request = URLRequest(url: url)
-//        let user = "fx33"
-//        let password = "42e2be4f5d638fcc5627c4b115c2a89d7d7dd8c949f63b3929a68a3b69335e40"
-//        let loginData = String(format: "%@:%@", user, password).data(using: .utf8)!
-//        let base64LoginData = loginData.base64EncodedString()
-//        request.addValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
-        let session = URLSession.shared
-        let task = session.downloadTask(with: request){
-            loc, resp, err in
-            let decoder = JSONDecoder()
-            var chats = [Chat]()
-            if let err = err{
-                print("Error: \(err)")
-//                if let _ = self.loadJSON(){
-//                    print("Success")
-//                }
-//                else{
-//                    print("Cannot find resources")
-//                    DispatchQueue.main.async {
-//                        self.records = self.loadCohort()
-//                    }
-//                    let _ = self.saveJSON()
-//                }
-            }
-            else if let loc = loc, let d = try? Data(contentsOf: loc)
-            {
-                do {
-                    let decoded = try decoder.decode([Chat].self, from: d)
-                    chats = decoded
-                    self.record = chats
-//                    for chat in chats{
-//                        DispatchQueue.main.async {
-//                            if self.findPerson(dukeperson.netID) != nil{
-//                                let _ = self.updatePerson_local(dukeperson)
-//                            }
-//                            else{
-//                                let _ = self.addPerson(dukeperson)
-//                            }
-//                        }
-//                    }
-//                    let _ = self.saveJSON()
-                } catch {
-                    print("Error: \(error)")
+    func update(chats: [Chat]){
+        self.record = chats
+    }
+    
+    
+    func postChosenScript(scriptname: String, completion: @escaping (Result<Data, Error>) -> Void) {
+            let url = URL(string: postChosenScripturl)
+            let encoder = JSONEncoder()
+            let postMessage = ["uuid": GameInfo.id, "scriptName": scriptname]
+            do {
+                let chatData = try encoder.encode(postMessage)
+                if let jsonString = String(data: chatData, encoding: .utf8) {
+                    print("Encoded JSON:\n\(jsonString)")
                 }
+                var request = URLRequest(url: url!)
+                request.httpMethod = "POST"
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpBody = chatData
+                
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                    if let error = error {
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    guard let data = data else {
+                        let error = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                        completion(.failure(error))
+                        return
+                    }
+                    
+                    completion(.success(data))
+                }
+                
+                task.resume()
+                
+            } catch let error {
+                completion(.failure(error))
             }
+    }
+    
+    func postChat(username: String, lastMessageText: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        let url = URL(string: postChaturl)
+        let encoder = JSONEncoder()
+        let postMessage = ["uuid": GameInfo.id, "npcName": username, "userInput": lastMessageText]
+        do {
+            let chatData = try encoder.encode(postMessage)
+            if let jsonString = String(data: chatData, encoding: .utf8) {
+                print("Encoded JSON:\n\(jsonString)")
+            }
+            var request = URLRequest(url: url!)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = chatData
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                guard let data = data else {
+                    let error = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                    completion(.failure(error))
+                    return
+                }
+                
+                completion(.success(data))
+            }
+            
+            task.resume()
+            
+        } catch let error {
+            completion(.failure(error))
         }
-        task.resume()
     }
 }
