442512521ad5b3f7c4820d4b34376844f3386b68
sq44 <sq44@duke.edu>
2023-03-25 23:59:36 -0400

    test secret

--- back_end/mysite/game/urls.py
+++ back_end/mysite/game/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('start', views.start, name='start'),
     path('chat_with_npc', views.chat_with_npc, name='chat_with_npc'),
-    path('add_game', views.add_game, name='add_game')
+    path('add_game', views.add_game, name='add_game'),
+    #path('chat_with_npc/<str:ride_id>/<str:ride_id>', views.chat_with_npc, name='chat_with_npc'),
+    path('test', views.test, name='test')
 ]
--- back_end/mysite/game/views.py
+++ back_end/mysite/game/views.py
@@ -5,8 +5,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import JsonResponse
 import openai
 import uuid
+
+from django.views.decorators.csrf import csrf_exempt
 
 from . import passwd
 
@@ -18,25 +21,39 @@ conversation = [{"role": "system", "content": "You are the roles of Princess Dra
                                               "Murder on the Orient Express, "
                                               "you need to answer my questions in her tongue"}]
 
+
 def start(request):
     # get script from scriptList; and also the uuid
-    json_data = '[{"name": "Alice", "age": 30}, {"name": "Bob", "age": 25}]'
-    data = json.loads(json_data)
     uuid_str = str(uuid.uuid4())
-    data.append("uuid", uuid_str)
+    json_data = "{}"
+    data = json.loads(json_data)
+    data["uuid"] = uuid_str
     json_data = json.dumps(data)
 
     return HttpResponse(json_data, content_type='application/json')
 
 
+@csrf_exempt
 def chat_with_npc(request):
-    conversation.append({"role": "user", "content": input("user: ")})
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=conversation
-    )
-    conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-    return HttpResponse(response['choices'][0]['message']['content'])
+    if request.method == 'POST':
+        # Handle the POST request data
+        msg = request.POST.get('message')
+        conversation.append({"role": "user", "content": msg}) # todo: store in database!
+        reply = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation
+        )
+        conversation.append({"role": "assistant", "content": reply['choices'][0]['message']['content']})
+
+        json_data = "{}"
+        data = json.loads(json_data)
+        data["content"] = reply['choices'][0]['message']['content']
+        return JsonResponse(data, status=200)
+    else:
+        # Handle other HTTP methods
+        response_data = {'message': 'This endpoint only accepts POST requests.'}
+        return JsonResponse(response_data, status=405)
+
 
 
 def add_game(request):
@@ -47,3 +64,11 @@ def add_game(request):
     )
     conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
     return HttpResponse(response['choices'][0]['message']['content'])
+
+
+def test(request):
+    json_data = "{}"
+    data = json.loads(json_data)
+    data["name"] = "something, cool cool fangze"
+    response = JsonResponse(data, status=200)
+    return response
